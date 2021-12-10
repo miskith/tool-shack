4709bf6fb4463d097fa549c852b8b82dd5d665c3
miskith <miskith@gmail.com>
2021-12-10 11:11:05 +0100

    Add isNil

---
+++ src/general/isNil.ts
@@ -0,0 +1,7 @@
+/**
+ * Check if value is equal to null or undefined
+ *
+ * @param value Value to be checked
+ * @returns Boolean indicating if value is equal to null or undefined
+ */
+export const isNil = (value: any): boolean => value === undefined || value === null;
