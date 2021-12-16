b8a08b2108b83a458806cc2fb7ea4869f7857fcb
miskith <miskith@gmail.com>
2021-12-16 20:00:07 +0100

    Add slugify function

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,3 +1,4 @@
 export * from './escapeHTML';
 export * from './removeDiacritics';
+export * from './slugify';
 export * from './truncate';
---
+++ src/string/slugify.ts
@@ -0,0 +1,17 @@
+import { removeDiacritics } from './removeDiacritics';
+
+/**
+ * Create slug from given string
+ *
+ * @param value String to be slugified
+ * @param separator One character to be used as separator (if provided longer than one character, will be automatically shortened), dash ('-') by default
+ * @returns Slugified string
+ */
+export const slugify = (value: string, separator: string = '-'): string => {
+  separator = separator.trim().substring(0, 1) || '-';
+  const removeCharsRegex = new RegExp(`[^\\\w\\\s${separator}]`, 'g');
+
+  return removeDiacritics(value.trim().toLowerCase())
+    .replace(removeCharsRegex, '')
+    .replace(/\s+/g, separator);
+};
