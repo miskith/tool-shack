f997d99c12e7c43b5d9a695d1f17428dcee353de
miskith <miskith@gmail.com>
2026-09-02 09:24:30 +0200

    Fix

--- src/browser/isTouchSupported.ts
+++ src/browser/isTouchSupported.ts
@@ -6,6 +6,8 @@ import { IExtendedWindow } from './interfaces/isTouchSupported.js';
  * @returns Boolean indicating if user's browser supports touch events
  */
 export const isTouchSupported = (): boolean =>
-  'ontouchstart' in window ||
-  ((window as unknown as IExtendedWindow).DocumentTouch &&
-    document instanceof (window as unknown as IExtendedWindow).DocumentTouch);
+  !!(
+    'ontouchstart' in window ||
+    ((window as unknown as IExtendedWindow).DocumentTouch &&
+      document instanceof (window as unknown as IExtendedWindow).DocumentTouch)
+  );
